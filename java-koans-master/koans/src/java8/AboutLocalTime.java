7bd86ba83899ceef01f0d9b5b7f52b4d9f1d9c02
nkalupahana <nkalupahana@users.noreply.github.com>
2016-11-09 13:56:07 -0800

    Updated LocalTime.java with newfound knowledge
    Un-did bypass after learning solution

--- java-koans-master/koans/src/java8/AboutLocalTime.java
+++ java-koans-master/koans/src/java8/AboutLocalTime.java
@@ -6,25 +6,21 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
-import static com.sandwich.util.Assert.*;
+import static com.sandwich.util.Assert.assertEquals;
 
 public class AboutLocalTime {
 
     @Koan
     public void localTime() {
         LocalTime t1 = LocalTime.of(7, 30);
-        //assertEquals(t1, LocalTime.parse("7:30"));
-        assertTrue(true);
+        assertEquals(t1, LocalTime.parse("07:30"));
     }
 
     @Koan
     public void localTimeMinus() {
         LocalTime t1 = LocalTime.parse("10:30");
         LocalTime t2 = t1.minus(2, ChronoUnit.HOURS);
-        //assertEquals(t2, LocalTime.parse("8:30"));
-        assertTrue(true);
+        assertEquals(t2, LocalTime.parse("08:30"));
     }
 
 }
-
-// Koans bypassed due to unpassable error
